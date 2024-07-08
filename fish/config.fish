d41343a5ae50ad0753567d2acb38e700261c3ac5
日比龍石 <dgelzex@hibiryosuke-Mac-mini.local>
2024-07-08 21:02:30 +0900

    fish configの追加

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+#set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+
+if command -v pyenv >/dev/null 2>&1
+    #pyenv init - | source
+end
+
+if status --is-interactive
+    # Commands to run in interactive sessions can go here
+    set PATH /opt/homebrew/bin $PATH
+    
+    #set -x PATH $HOME/.anyenv/bin $PATH
+    #anyenv init - fish | source
+end
+
+source /Users/dgelzex/.config/fish/user/abbr.fish
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+zoxide init fish | source
---
+++ fish/user/abbr.fish
@@ -0,0 +1,19 @@
+# 汎用設定
+abbr cl clear
+abbr rf exec fish
+abbr rz exec zsh
+
+#docker compose 周り
+abbr dcup docker compose up -d
+abbr ddown docker compose down
+abbr mcli docker exec -i 1165_forge_wf rcon-cli
+abbr dps docker ps
+
+# git周り
+abbr gin git init
+abbr gst git status
+abbr gcm git commit -m ""
+abbr gad git add
+abbr gme git merge
+abbr gsw git switch
+abbr gbr git branch
