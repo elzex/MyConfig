68a47cb1a6899ab0fbdce4ff17264f29c6beac03
elzex <dgel.zex@gmail.com>
2024-07-09 21:29:33 +0900

    linux用設定分離

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,10 @@
 if status --is-interactive
     # Commands to run in interactive sessions can go here
-    set PATH /opt/homebrew/bin $PATH
+    set PATH /home/linuxbrew/.linuxbrew/bin $PATH
 end
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+source /Users/dgelzex/.config/fish/user/abbr.fish
 
 zoxide init fish | source
 
-set -gx PATH $PATH /opt/nvim-linux64/bin
-
 set -U FZF_LEGACY_KEYBINDINGS 0
-
-switch (uname)
-    case Darwin
-        source /opt/homebrew/opt/asdf/libexec/asdf.fish
-        source /Users/dgelzex/.config/fish/user/abbr.fish
-    case Linux
-        source ~/.asdf/asdf.fish
-        source ~/.config/fish/user/abbr.fish
-end
