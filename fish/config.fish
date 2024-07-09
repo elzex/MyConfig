8b49930b2d14f2c2a11eaa4b3e290617117a2dcb
Ryosuke Hibi <dgel.zex@gmail.com>
2024-07-09 21:46:01 +0900

    微修正

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,9 @@ if status --is-interactive
     # Commands to run in interactive sessions can go here
     set PATH /home/linuxbrew/.linuxbrew/bin $PATH
 end
+
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
-source /Users/dgelzex/.config/fish/user/abbr.fish
+source /home/dgelzex/.config/fish/user/abbr.fish
 
 zoxide init fish | source
 
