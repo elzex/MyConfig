255f1a3a5d2b82c70bff8f898f8eb1ebc9cff43f
*wd8cbPukM9 <r_hibi@webfrontier.co.jp>
2024-07-09 09:10:19 +0900

    開発環境の更新

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,14 @@
-#set -x PYENV_ROOT $HOME/.pyenv
-set -x PATH $PYENV_ROOT/bin $PATH
-
-if command -v pyenv >/dev/null 2>&1
-    #pyenv init - | source
-end
-
 if status --is-interactive
     # Commands to run in interactive sessions can go here
     set PATH /opt/homebrew/bin $PATH
-    
-    #set -x PATH $HOME/.anyenv/bin $PATH
-    #anyenv init - fish | source
 end
-
-source /Users/dgelzex/.config/fish/user/abbr.fish
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 zoxide init fish | source
+
+set -gx PATH $PATH /opt/nvim-linux64/bin
+
+set -U FZF_LEGACY_KEYBINDINGS 0
+
+source /Users/dgelzex/.config/fish/user/abbr.fish
