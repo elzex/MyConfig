b3fd7e278c356030a78888222a8a6a91f437ad3f
elzex <dgel.zex@gmail.com>
2024-07-22 20:19:01 +0900

    asdfの読み込み順を修正

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,13 @@
 if status --is-interactive
     # Commands to run in interactive sessions can go here
-    set PATH /opt/homebrew/bin $PATH
 end
+
+set PATH /opt/homebrew/bin $PATH
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+source /Users/dgelzex/.config/fish/user/abbr.fish
 
 zoxide init fish | source
 
 set -gx PATH $PATH /opt/nvim-linux64/bin
 
 set -U FZF_LEGACY_KEYBINDINGS 0
-
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
-source /Users/dgelzex/.config/fish/user/abbr.fish
-
