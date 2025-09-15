14870241d5df9f07f4f98dea4049418110e96b0b
dgelzex <dgel.zex@gmail.com>
2025-09-15 21:50:43 +0900

    Revert "[update]新環境に変更したため、それに合わせて.configを更新"
    This reverts commit 24e4019967eaff8c0fe15ad94c9184e17f9161bd.

--- fish/config.fish
+++ fish/config.fish
@@ -3,20 +3,7 @@ if status --is-interactive
     set PATH /home/linuxbrew/.linuxbrew/bin $PATH
 end
 
-# ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-
+source /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.fish
 source ~/.config/fish/user/abbr.fish
 source ~/.config/fish/user/alias.fish
 
